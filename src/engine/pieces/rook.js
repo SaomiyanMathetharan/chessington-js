7836f8f96197fce495db466079f2f512e08e3686
Saomiyan Mathetharan <Saomiyan.Mathetharan@softwire.com>
2022-06-27 12:11:09 +0100

    Added rook movement

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +7,14 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const availableMoves = [];
+        const fromPosition = board.findPiece(this);
+
+        for (let i = 0; i < board.board.length; i++) {
+            availableMoves.push(new Square(i, fromPosition.col));
+            availableMoves.push(new Square(fromPosition.row, i));
+        }
+
+        return availableMoves.filter(square => !square.equals(fromPosition));
     }
 }
