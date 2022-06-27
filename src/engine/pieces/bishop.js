4293b03a996b7793aae5c63581179dad2ee23e46
Saomiyan Mathetharan <Saomiyan.Mathetharan@softwire.com>
2022-06-27 12:24:28 +0100

    Added diagonal movement for Bishop

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +7,31 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let availableMoves = [];
+        const fromPosition = board.findPiece(this);
+
+        availableMoves = this.concatAvailableMoves(
+            this.diagonalAvailableMoves(board, fromPosition, 1, 1),
+            this.diagonalAvailableMoves(board, fromPosition, -1, 1),
+            this.diagonalAvailableMoves(board, fromPosition, 1, -1),
+            this.diagonalAvailableMoves(board, fromPosition, -1, -1)
+        )
+
+        return availableMoves.filter(square => !square.equals(fromPosition));
+    }
+
+    diagonalAvailableMoves(board, fromPosition, xDirection, yDirection) {
+        let row = fromPosition.row;
+        let col = fromPosition.col;
+        const size = board.board.length;
+
+        const availableMoves = [];
+        while (0 <= row && row < size && 0 <= col && col < size) {
+            availableMoves.push(new Square(row, col));
+            row += yDirection;
+            col += xDirection;
+        }
+
+        return availableMoves;
     }
 }
