a2c6605b7fb205de219f0c9c9189fc02bd0b60a2
Saomiyan Mathetharan <Saomiyan.Mathetharan@softwire.com>
2022-06-27 15:48:09 +0100

    Added blocking functionality for rook, bishop, king & queen

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -23,17 +23,35 @@ export default class Piece {
             availableMoves.push(new Square(fromPosition.row, i));
         }
 
-        return availableMoves.filter(square => !square.equals(fromPosition));
+        const upClosestPiece = this.findClosestPiece(fromPosition, board, 0, -1);
+        const downClosestPiece = this.findClosestPiece(fromPosition, board, 0, 1);
+        const leftClosestPiece = this.findClosestPiece(fromPosition, board, -1, 0);
+        const rightClosestPiece = this.findClosestPiece(fromPosition, board, 1, 0);
+
+        return availableMoves.filter(square => !square.equals(fromPosition))
+            .filter(square => leftClosestPiece.col < square.col && square.col < rightClosestPiece.col)
+            .filter(square => upClosestPiece.row < square.row && square.row < downClosestPiece.row);
     }
 
     getAllDiagonalAvailableMoves(board) {
         const fromPosition = board.findPiece(this);
 
-        return this.diagonalAvailableMoves(board, fromPosition, 1, 1)
+        const upLeftClosestPiece = this.findClosestPiece(fromPosition, board, -1, -1);
+        const upRightClosestPiece = this.findClosestPiece(fromPosition, board, 1, -1);
+        const downLeftClosestPiece = this.findClosestPiece(fromPosition, board, -1, 1);
+        const downRightClosestPiece = this.findClosestPiece(fromPosition, board, 1, 1);
+
+        const a = this.diagonalAvailableMoves(board, fromPosition, 1, 1)
             .concat(this.diagonalAvailableMoves(board, fromPosition, -1, 1))
             .concat(this.diagonalAvailableMoves(board, fromPosition, 1, -1))
             .concat(this.diagonalAvailableMoves(board, fromPosition, -1, -1))
-            .filter(square => !square.equals(fromPosition));
+            .filter(square => !square.equals(fromPosition))
+            .filter(square => upLeftClosestPiece.col < square.col || upLeftClosestPiece.row < square.row)
+            .filter(square => square.col < upRightClosestPiece.col || upRightClosestPiece.row < square.row)
+            .filter(square => downLeftClosestPiece.col < square.col || square.row < downLeftClosestPiece.row)
+            .filter(square => square.col < downRightClosestPiece.col || square.row < downRightClosestPiece.row);
+        console.log(a);
+        return a;
     }
 
     diagonalAvailableMoves(board, fromPosition, xDirection, yDirection) {
@@ -53,6 +71,21 @@ export default class Piece {
 
     boundsCheck(row, col, size) {
         return 0 <= row && row < size && 0 <= col && col < size;
+    }
+
+    findClosestPiece(currentPosition, board, xDirection, yDirection) {
+        let row = currentPosition.row + yDirection;
+        let col = currentPosition.col + xDirection;
+
+        while (this.boundsCheck(row, col, board.board.length)) {
+            if (typeof board.getPiece(Square.at(row, col)) !== "undefined") {
+                break;
+            }
+            row += yDirection;
+            col += xDirection;
+        }
+
+        return new Square(row, col);
     }
 
 }
